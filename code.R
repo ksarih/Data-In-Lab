74b4b7b4ebbeb166a0179c5fe7e7151ebe8ed023
ksarih <sarihkaoutar@gmail.com>
2025-11-25 17:31:02 +0100

    test multiple et stratification

--- code.R
+++ code.R
@@ -116,5 +116,281 @@ IC_sup
 
 
 ####################
+#stratification
+table_sepsis <- table(data$Favorable, data$INCL_SEPSIS_YN)
+table_sepsis
+
+table_age <- table(data$Favorable, data$AGE_CLASS)
+table_age
+table_akin <- table(data$Favorable, data$INCL_AKIN)
+table_akin
+library(ggplot2)
+df_plot <- data.frame(
+  Groupe = c("Sans sepsis", "Avec sepsis",
+             "< 65 ans", "≥ 65 ans",
+             "AKIN 0–1", "AKIN 2–3"),
+  Pourcentage = c(
+    31/(31+133)*100, 36/(36+200)*100,
+    25/(25+170)*100, 42/(42+163)*100,
+    28/(28+183)*100, 39/(39+150)*100
+  ),
+  Type = c("Sepsis", "Sepsis",
+           "Âge", "Âge",
+           "AKIN", "AKIN")
+)
+ggplot(df_plot %>% filter(Type == "Sepsis"),
+       aes(x = Groupe, y = Pourcentage)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  ylim(0, 30) +
+  labs(title = "Proportion de Favorables selon le Sepsis",
+       x = "",
+       y = "Proportion de Favorables (%)") +
+  theme_minimal(base_size = 14)
+
+ggplot(df_plot %>% filter(Type == "Âge"),
+       aes(x = Groupe, y = Pourcentage)) +
+  geom_bar(stat = "identity", fill = "darkgreen") +
+  ylim(0, 30) +
+  labs(title = "Proportion de Favorables selon l'âge",
+       x = "",
+       y = "Proportion de Favorables (%)") +
+  theme_minimal(base_size = 14)
+
+ggplot(df_plot %>% filter(Type == "AKIN"),
+       aes(x = Groupe, y = Pourcentage)) +
+  geom_bar(stat = "identity", fill = "firebrick") +
+  ylim(0, 30) +
+  labs(title = "Proportion de Favorables selon le stade AKIN",
+       x = "",
+       y = "Proportion de Favorables (%)") +
+  theme_minimal(base_size = 14)
 
 
+# Sepsis = 0
+tab_sepsis0 <- table(
+  data$Favorable[data$INCL_SEPSIS_YN == 0],
+  data$ARM_NUM[data$INCL_SEPSIS_YN == 0]
+)
+tab_sepsis0
+
+p_sepsis0 <- if(any(tab_sepsis0 < 5)) {
+  fisher.test(tab_sepsis0)$p.value
+} else {
+  chisq.test(tab_sepsis0)$p.value
+}
+
+# Sepsis = 1
+tab_sepsis1 <- table(
+  data$Favorable[data$INCL_SEPSIS_YN == 1],
+  data$ARM_NUM[data$INCL_SEPSIS_YN == 1]
+)
+tab_sepsis1
+
+p_sepsis1 <- if(any(tab_sepsis1 < 5)) {
+  fisher.test(tab_sepsis1)$p.value
+} else {
+  chisq.test(tab_sepsis1)$p.value
+}
+# Age < 65
+tab_age0 <- table(
+  data$Favorable[data$AGE_CLASS == 0],
+  data$ARM_NUM[data$AGE_CLASS == 0]
+)
+tab_age0
+
+p_age0 <- if(any(tab_age0 < 5)) {
+  fisher.test(tab_age0)$p.value
+} else {
+  chisq.test(tab_age0)$p.value
+}
+
+# Age ≥ 65
+tab_age1 <- table(
+  data$Favorable[data$AGE_CLASS == 1],
+  data$ARM_NUM[data$AGE_CLASS == 1]
+)
+tab_age1
+
+p_age1 <- if(any(tab_age1 < 5)) {
+  fisher.test(tab_age1)$p.value
+} else {
+  chisq.test(tab_age1)$p.value
+}
+
+
+# AKIN 0–1
+tab_akin0 <- table(
+  data$Favorable[data$INCL_AKIN == 0],
+  data$ARM_NUM[data$INCL_AKIN == 0]
+)
+tab_akin0
+
+p_akin0 <- if(any(tab_akin0 < 5)) {
+  fisher.test(tab_akin0)$p.value
+} else {
+  chisq.test(tab_akin0)$p.value
+}
+
+# AKIN 2–3
+tab_akin1 <- table(
+  data$Favorable[data$INCL_AKIN == 1],
+  data$ARM_NUM[data$INCL_AKIN == 1]
+)
+tab_akin1
+
+p_akin1 <- if(any(tab_akin1 < 5)) {
+  fisher.test(tab_akin1)$p.value
+} else {
+  chisq.test(tab_akin1)$p.value
+}
+
+
+
+p_traitement_strat <- c(
+  p_sepsis0, p_sepsis1,
+  p_age0, p_age1,
+  p_akin0, p_akin1
+)
+
+p_traitement_strat
+
+# Correction Bonferroni (6 tests)
+p_bonf_trait <- p.adjust(p_traitement_strat, method = "bonferroni")
+
+# Correction Benjamini–Hochberg
+p_bh_trait <- p.adjust(p_traitement_strat, method = "BH")
+
+resultats_traitement_strat <- data.frame(
+  Stratification = c("Sepsis=0", "Sepsis=1", "Âge<65", "Âge≥65", "AKIN 0–1", "AKIN 2–3"),
+  p_brut = p_traitement_strat,
+  p_bonferroni = p_bonf_trait,
+  p_BH = p_bh_trait
+)
+
+resultats_traitement_strat
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######age x sepsis
+
+data$AGE_SEPSIS <- interaction(data$AGE_CLASS, data$INCL_SEPSIS_YN)
+table(data$AGE_SEPSIS)
+p_values_age_sepsis <- c()
+
+for (g in unique(data$AGE_SEPSIS)) {
+  cat("\nGROUPE :", g, "\n")
+  
+  tab <- table(
+    data$Favorable[data$AGE_SEPSIS == g],
+    data$ARM_NUM[data$AGE_SEPSIS == g]
+  )
+  
+  print(tab)
+  
+  p_values_age_sepsis <- c(
+    p_values_age_sepsis,
+    if(any(tab < 5)) fisher.test(tab)$p.value else chisq.test(tab)$p.value
+  )
+}
+
+p_values_age_sepsis
+p.adjust(p_values_age_sepsis, method = "BH")
+############### age x akin
+data$AGE_AKIN <- interaction(data$AGE_CLASS, data$INCL_AKIN)
+table(data$AGE_AKIN)
+p_values_age_akin <- c()
+
+for (g in unique(data$AGE_AKIN)) {
+  cat("\nGROUPE :", g, "\n")
+  
+  tab <- table(
+    data$Favorable[data$AGE_AKIN == g],
+    data$ARM_NUM[data$AGE_AKIN == g]
+  )
+  
+  print(tab)
+  
+  p_values_age_akin <- c(
+    p_values_age_akin,
+    if(any(tab < 5)) fisher.test(tab)$p.value else chisq.test(tab)$p.value
+  )
+}
+
+p_values_age_akin
+p.adjust(p_values_age_akin, method = "BH")
+
+
+
+
+
+
+
+
+
+####GRAPHES 
+df_age_sepsis <- data %>% 
+  group_by(AGE_SEPSIS, ARM_NUM) %>% 
+  summarise(
+    Favorables = sum(Favorable),
+    Total = n(),
+    Proportion = Favorables / Total * 100
+  )
+df_age_sepsis$Groupe <- factor(df_age_sepsis$AGE_SEPSIS,
+                               levels = c("0.0","0.1","1.0","1.1"),
+                               labels = c("<65 ans / sans sepsis",
+                                          "<65 ans / avec sepsis",
+                                          "≥65 ans / sans sepsis",
+                                          "≥65 ans / avec sepsis")
+)
+ggplot(df_age_sepsis, aes(x = Groupe, y = Proportion, fill = factor(ARM_NUM))) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8)) +
+  labs(title = "Proportion de Favorables selon Âge × Sepsis",
+       x = "",
+       y = "Proportion de Favorables (%)",
+       fill = "Traitement (ARM_NUM)") +
+  theme_minimal(base_size = 14) +
+  theme(axis.text.x = element_text(angle = 15, hjust = 1))
+df_age_akin <- data %>% 
+  group_by(AGE_AKIN, ARM_NUM) %>% 
+  summarise(
+    Favorables = sum(Favorable),
+    Total = n(),
+    Proportion = Favorables / Total * 100
+  )
+df_age_akin$Groupe <- factor(df_age_akin$AGE_AKIN,
+                             levels = c("0.0","0.1","1.0","1.1"),
+                             labels = c("<65 ans / AKIN 0–1",
+                                        "<65 ans / AKIN 2–3",
+                                        "≥65 ans / AKIN 0–1",
+                                        "≥65 ans / AKIN 2–3")
+)
+ggplot(df_age_akin, aes(x = Groupe, y = Proportion, fill = factor(ARM_NUM))) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8)) +
+  labs(title = "Proportion de Favorables selon Âge × AKIN",
+       x = "",
+       y = "Proportion de Favorables (%)",
+       fill = "Traitement (ARM_NUM)") +
+  theme_minimal(base_size = 14) +
+  theme(axis.text.x = element_text(angle = 15, hjust = 1))
